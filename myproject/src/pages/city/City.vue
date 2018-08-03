6be54ce9b6023005da055cf62cc6ca5bb41b14e4
CAICAISKY <37895043+CAICAISKY@users.noreply.github.com>
2018-08-03 08:48:36 +0800

    city-alphabet

--- myproject/src/pages/city/City.vue
+++ myproject/src/pages/city/City.vue
@@ -3,6 +3,7 @@
     <city-header></city-header>
     <city-search></city-search>
     <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import CityHeader from '@/pages/city/components/Header'
 import CitySearch from '@/pages/city/components/Search'
 import CityList from '@/pages/city/components/List'
+import CityAlphabet from '@/pages/city/components/Alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
     CitySearch,
-    CityList
+    CityList,
+    CityAlphabet
   }
 }
 </script>
---
+++ myproject/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+   @import '~styles/varibles.styl'
+  .list
+    display: flex             //弹性盒子模式
+    flex-direction: column    //弹性盒子模式：按列排序；row为按行排序
+    justify-content: center   //弹性盒子模式：置中
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
