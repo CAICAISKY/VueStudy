473d9c86775c6a4b7806e3bb71369c692ffa9e7d
Schuyler <caishunweiking@126.com>
2018-08-04 14:43:13 +0800

    city-axios

--- myproject/src/pages/city/City.vue
+++ myproject/src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from '@/pages/city/components/Header'
 import CitySearch from '@/pages/city/components/Search'
 import CityList from '@/pages/city/components/List'
 import CityAlphabet from '@/pages/city/components/Alphabet'
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
@@ -19,6 +20,28 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityListInfo () {
+      axios.get('/api/city.json').then(this.handleCityListInfo)
+    },
+    handleCityListInfo (response) {
+      response = response.data
+      if (response.ret && response.data) {
+        const data = response.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityListInfo()
   }
 }
 </script>
