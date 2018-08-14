b946a2a6bce5f3e14390dc8b3ff9bcc1fdf559b5
Schuyler <caishunweiking@126.com>
2018-08-14 08:39:26 +0800

    finish project

--- myproject/src/router/index.js
+++ myproject/src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -10,14 +7,14 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Home
+    component: () => import('@/pages/home/Home')
   }, {
     path: '/city',
     name: 'city',
-    component: City
+    component: () => import('@/pages/city/City')
   }, {
     path: '/detail/:id',
     name: 'Detail',
-    component: Detail
+    component: () => import('@/pages/detail/Detail')
   }]
 })
